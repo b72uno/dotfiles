7ecaa26c6d6cbc324f0e4235603c048fa2f9ca34
Bruno Berzins <b72uno@gmail.com>
2018-05-13 02:17:40 +0300

    added script to switch gcc

--- scripts/switch_gcc.sh
+++ scripts/switch_gcc.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-# Source https://unix.stackexchange.com/questions/218557/how-to-change-ownership-from-symbolic-links#218559
+# Source - https://askubuntu.com/questions/26498/choose-gcc-and-g-version
+# =========================================================================
+# First, remove old packages
+# sudo update-alternatives --remove-all gcc 
+# sudo update-alternatives --remove-all g++
+
+# Install packages
+# sudo apt-get install gcc-4.3 gcc-4.4 g++-4.3 g++-4.4
+
+# Install alternatives
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.3 10
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.4 20
+
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.3 10
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.4 20
+
+# sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+# sudo update-alternatives --set cc /usr/bin/gcc
+
+# sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+# sudo update-alternatives --set c++ /usr/bin/g++
+
+# use this cript to switch alternatives
 
 if [ -z "$1" ]; then
     echo "usage: $0 version" 1>&2
